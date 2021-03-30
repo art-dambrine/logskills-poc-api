91e9d32725e5ce12344c7176907391fc34898fe6
jb.cahier <jb.cahier@gmail.com>
2021-03-30 11:00:29 +0200

    Ajout des insert dans le setup.sql

--- database-service/setup.sql
+++ database-service/setup.sql
@@ -55,3 +55,30 @@ ALTER TABLE `Tags_Activites` ADD FOREIGN KEY (`id_activite`) REFERENCES `Activit
 
 ALTER TABLE `Tags_Activites` ADD FOREIGN KEY (`id_tag`) REFERENCES `Tags` (`id_tag`);
 
+/* INSERT DATA */
+INSERT INTO Utilisateurs (nom, prenom, mail, login, password, cree_le, modifie_le)
+VALUES ('SYS', 'ADMIN', 'sysadmin@test.fr', 'SYSADM', '123', curdate(), curdate());
+
+INSERT INTO Tags (nom)
+VALUES ('Projet');
+
+INSERT INTO Categories (nom)
+VALUES ('Etudes');
+
+INSERT INTO Activites (nom, temps_focus, temps_pause, nb_round, id_categorie, id_utilisateur, cree_le, modifie_le)
+VALUES ('Code Java', 25, 5, 3, 1, 1, curdate(), curdate());
+
+INSERT INTO Tags_Activites (id_tag, id_activite)
+VALUES (1,1);
+
+INSERT INTO Log_Activites (temps_total, temps_actif, id_activite, date)
+VALUES (90, 75, 1, curdate());
+
+INSERT INTO Categories (nom)
+VALUES ('Loisirs');
+INSERT INTO Categories (nom)
+VALUES ('Travails');
+INSERT INTO Categories (nom)
+VALUES ('Codes');
+INSERT INTO Categories (nom)
+VALUES ('Sports');
